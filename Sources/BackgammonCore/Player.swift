1d0ea113dd950ad12a1f490a79d854809550e22f
Nicolas Baez <89105332+Nick-qsv@users.noreply.github.com>
2025-06-02 16:38:41 -0400

    player?

--- Sources/BackgammonCore/Player.swift
+++ Sources/BackgammonCore/Player.swift
@@ -1,1 +1,13 @@
 
+public enum Player: Codable, Hashable {
+    case player1
+    case player2
+    var name: String {
+        switch self {
+        case .player1:
+            return "Player 1"
+        case .player2:
+            return "Player 2"
+        }
+    }
+}
